53d321ef8f3ed58af8e800581baa591b102523be
Stephanie Lascola <slascola@calpoly.edu>
2017-01-16 22:05:40 -0800

    Update Sorts.java

--- Sorts/Sorts.java
+++ Sorts/Sorts.java
@@ -43,4 +43,81 @@ public class Sorts {
 			original[end] = temp[end];
 		}
 	}
+	
+	public static void selectionSort(int[] arr, int N) {
+		for (int i = 0; i < N - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < N; j++) {
+				if (arr[j] < arr[index]) {
+					index = j;	
+				}
+			}
+			int smallerNumber = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallerNumber;
+		}
+	}
+
+	public static void quickSort(int[] arr, int N) {	
+		quickSort(arr, 0, N-1);
+	}
+
+	private static void quickSort(int[] arr, int first, int last) {
+		if (first < last) {
+			setPivotToEnd(arr, first, last);
+			int pivotIndex = splitList(arr, first, last);
+			quickSort(arr, first, pivotIndex - 1);
+			quickSort(arr, pivotIndex + 1, last);
+		}
+	}
+
+	private static void setPivotToEnd(int[] arr, int left, int right) {	
+		int center = (left + right) / 2;
+		int temp = 0;
+
+		if (arr[center] < arr[left]) {
+			temp = arr[center];
+			arr[center] = arr[left];
+			arr[left] = temp;
+		}
+		if (arr[right] < arr[left]) {
+			temp = arr[right];
+			arr[right] = arr[left];
+			arr[left] = temp;
+		}
+		if (arr[right] < arr[center]) {
+			temp = arr[right];
+			arr[right] = arr[center];
+			arr[center] = temp;
+		}
+		//Return pivot to end
+		temp = arr[center];
+		arr[center] = arr[right - 1];
+		arr[right - 1] = temp;
+		
+	}
+	
+	private static int splitList(int arr[], int first, int last) {
+		int i = first;
+		int j = last;
+		int temp;
+		int pivot = arr[last];
+
+		while (true) {
+			while (arr[++i] < pivot) {}
+			while (arr[--j] > pivot) {}
+			if (i < j) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j]= temp;
+			}
+			else {
+				break;
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[last];
+		arr[last] = temp;
+		return i;	
+	}
 }
