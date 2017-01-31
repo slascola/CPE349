fc91a5dbadcc7375dc4aae66d229d2b33e9fe9a3
Stephanie Lascola <slascola@calpoly.edu>
2017-01-30 21:39:47 -0800

    Update MatrixProduct.java

--- MatrixProduct.java
+++ MatrixProduct.java
@@ -17,41 +17,31 @@ public class MatrixProduct {
 		int[][] C = new int[n][n];
 		int[][] a1, a2, a3, a4, b1, b2, b3, b4;
 		if (n == 1) {
-			C[1][1] = A[startRowA][startColA] * B[startRowB][startColB];
+			C[0][0] = A[startRowA][startColA] * B[startRowB][startColB];
 		}
 		else {
 			a1 = partition(A,1); 
-			a2 = partition(A, 2);
-			a3 = partition(A, 3);
-			a4 = partition(A, 4);
-			
-		 System.out.println("Partition Matrix:");
+			a2 = partition(A,2);
+			a3 = partition(A,3);
+			a4 = partition(A,4);
+			b1 = partition(B,1); 
+			b2 = partition(B,2);
+			b3 = partition(B,3);
+			b4 = partition(B,4);
+			//Recursive calls based off 0 or 1/2 of matrix
+			addMatrices(matrixProduct_DAC(A, 0, 0, B, 0, 0, n/2), matrixProduct_DAC(A, 0, n/2, B, n/2, 0, n/2));
+			addMatrices(matrixProduct_DAC(A, 0, 0, B, 0, n/2, n/2), matrixProduct_DAC(A, 0, n/2, B, n/2, n/2, n/2));
+			addMatrices(matrixProduct_DAC(A, n/2, 0, B, 0, 0, n/2), matrixProduct_DAC(A, n/2, n/2, B, n/2, 0, n/2));
+			addMatrices(matrixProduct_DAC(A, n/2, 0, B, 0, n/2, n/2), matrixProduct_DAC(A, n/2, n/2, B, n/2, n/2, n/2));
+		
+		
+			//Testing for partitioning
+			System.out.println("Partition Matrix:");
 	        for (int i = 0; i < a1.length; i++) {
 	            for (int j = 0; j < a1[0].length; j++) {
 	                System.out.print(a1[i][j] + " ");
 	            }
-	            System.out.println();
-	        }
-	        System.out.println("Partition Matrix:");
-	        for (int i = 0; i < a2.length; i++) {
-	            for (int j = 0; j < a2[0].length; j++) {
-	                System.out.print(a2[i][j] + " ");
-	            }
-	            System.out.println();
-	        }
-	        System.out.println("Partition Matrix:");
-	        for (int i = 0; i < a3.length; i++) {
-	            for (int j = 0; j < a3[0].length; j++) {
-	                System.out.print(a3[i][j] + " ");
-	            }
-	            System.out.println();
-	        }
-	        System.out.println("Partition Matrix:");
-	        for (int i = 0; i < a4.length; i++) {
-	            for (int j = 0; j < a4[0].length; j++) {
-	                System.out.print(a4[i][j] + " ");
-	            }
-	            System.out.println();
+	        System.out.println();
 	        }
 		}
 		return C;
@@ -90,5 +80,8 @@ public class MatrixProduct {
 		return broken;
 	}
 	
+	private static void addMatrices(int[][] A, int[][]B, int[][] C) {
+		
+	}
 }
 
