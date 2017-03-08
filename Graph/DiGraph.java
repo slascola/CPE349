939e0b366c08dfb8ebd9739757e7e2d37a4a0131
aryakarnik <aishwarya.ark@gmail.com>
2017-03-08 13:14:38 -0800

    Update DiGraph.java

--- Graph/DiGraph.java
+++ Graph/DiGraph.java
@@ -3,39 +3,51 @@
 // Project 5
 
 import java.util.*;
+
 public class DiGraph {
-	private LinkedList<Integer> graph;
-	graph = new LinkedList<Integer>();
-	
-	public void addEdge(int from, int to) {
-		if (graph[from] == null) {
-			graph[from] = new LinkedList<Integer>();
-			grpah[from].add(to);
-		}
-		else {
-			graph[from].add(to);
-		}
-	}
-	
-	public void deleteEdge(int from, int to) {
-		if (!graph[from].contains(to)) {
-			System.out.println("Delete Edge Failed: No edge to be deleted");
-		}
-		else {
-			int deleted = graph[from].indexOf(to);
-			graph[from].remove(deleted);
-		}
-	}
-	public int edgeCount() {
-		return graph.length*(graph.length - 1);
-	}
-	public int vertexCount() {
-		return graph.length;
-	}
-	public void print() {
-		for (int i = 1; i < graph.length + 1; i++) {
-			System.out.println(i + " " + ":" + " " + graph[i]);
-		}
-	}
-	
+    private LinkedList<Integer>[] graph;
+
+    public DiGraph(int N) {
+        graph = new LinkedList[N];
+    }
+    
+    public void addEdge(int from, int to) {
+        from--;
+        to--;
+        if (!(graph[from].contains(to))) {
+            graph[from].add(to);
+        }
+    }
+    
+    public void deleteEdge(int from, int to) {
+        if (!graph[from].contains(to)) {
+            System.out.println("Delete Edge Failed: No edge to be deleted");
+        }
+        else {
+            int deleted = graph[from].indexOf(to);
+            graph[from].remove(deleted);
+        }
+    }
+    public int edgeCount() {
+        int totalEdges = 0;
+        for (int i = 0; i < graph.length; i++) {
+            totalEdges += graph[i].size();
+        }
+
+        return totalEdges;
+    }
+    public int vertexCount() {
+        return graph.length;
+    }
+    public void print() {
+        for (int i = 0; i < graph.length; i++) {
+            i++;
+            System.out.println(i + "is connected to: ");
+            i--;
+            for (int j = 0; j < graph[i].size(); j++) {
+                System.out.println(graph[i].get(j) + ", ");
+            }
+            System.out.println();
+        }
+    }
 }
